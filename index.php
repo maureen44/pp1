4bd0eb50e65391e98f1b8df938dc82cfc65cf48f
Maureen Kariuki <mkariuki2@mail.greenriver.edu>
2020-01-06 14:45:53 -0500

    Added a function to print an array of numbers

--- index.php
+++ index.php
@@ -14,5 +14,15 @@ error_reporting(E_ALL);
 </head>
 <body>
     <h1>Pair Program 1</h1>
+    <?php
+    $numbers = array(7,9,8,9,8,8,6);
+
+    function printArr($numbers) {
+        foreach ($numbers as $num) {
+            echo "<p>$num </P>";
+        }
+    }
+    printArr($numbers);
+    ?>
 </body>
 </html>
